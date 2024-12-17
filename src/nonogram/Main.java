f0345bdf52af432e057f17a33f07aed84d788f63
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-12-17 22:09:18 +0100

    Removed tests now covered with JUnit

--- src/nonogram/Main.java
+++ src/nonogram/Main.java
@@ -18,8 +18,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-        
-//        Board board = new Board(20, 20);
 
 //        ActiveNonogram Camera = new ActiveNonogram("Camera.bmp", 1);
 //        Board board = Camera.getBoard();
@@ -31,53 +29,6 @@ public class Main {
         Board board = CatPot_Large.getBoard();
         Solver solver = new Solver(board);
         solver.BeginSolving(1);
-
-//        System.out.println("\nUP");
-//        System.out.println("Column 0");
-//        solver.currentSideLength(Solver.direction.UP, 0);
-//        System.out.println("Column 1");
-//        solver.currentSideLength(Solver.direction.UP, 1);
-//        System.out.println("Column 2");
-//        solver.currentSideLength(Solver.direction.UP, 2);
-//        System.out.println("Column 18");
-//        solver.currentSideLength(Solver.direction.UP, 18);
-//        
-//        System.out.println("\nDOWN");
-//        System.out.println("Column 0");
-//        solver.currentSideLength(Solver.direction.DOWN, 0);
-//        System.out.println("Column 1");
-//        solver.currentSideLength(Solver.direction.DOWN, 1);
-//        System.out.println("Column 3");
-//        solver.currentSideLength(Solver.direction.DOWN, 3);
-//        System.out.println("Column 18");
-//        solver.currentSideLength(Solver.direction.DOWN, 18);
-//                
-//        System.out.println("\nRIGHT");
-//        System.out.println("Column 0");
-//        solver.currentSideLength(Solver.direction.RIGHT, 0);
-//        System.out.println("Column 1");
-//        solver.currentSideLength(Solver.direction.RIGHT, 1);
-//        System.out.println("Column 2");
-//        solver.currentSideLength(Solver.direction.RIGHT, 2);
-//        System.out.println("Column 18");
-//        solver.currentSideLength(Solver.direction.RIGHT, 18);
-//        
-//        System.out.println("\nLEFT");
-//        System.out.println("Column 0");
-//        solver.currentSideLength(Solver.direction.LEFT, 0);
-//        System.out.println("Column 1");
-//        solver.currentSideLength(Solver.direction.LEFT, 1);
-//        System.out.println("Column 3");
-//        solver.currentSideLength(Solver.direction.LEFT, 3);
-//        System.out.println("Column 18");
-//        solver.currentSideLength(Solver.direction.LEFT, 18);
-        
-//        board.fillSquare(0,0);
-//        board.crossSquare(1,0);
-//        System.out.println(board);
-//        System.out.println(Camera);
-        
-
         
     }
     
