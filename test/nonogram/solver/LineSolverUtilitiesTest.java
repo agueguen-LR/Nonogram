13665d7d7298db74d56cd34571cccb2b2ec4c52d
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-12-18 19:20:12 +0100

    Added more tests

--- test/nonogram/solver/LineSolverUtilitiesTest.java
+++ test/nonogram/solver/LineSolverUtilitiesTest.java
@@ -1,6 +1,8 @@
 package nonogram.solver;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import nonogram.generator.Clue;
 import nonogram.generator.Square;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,53 +13,146 @@ import java.util.List;
 
 public class LineSolverUtilitiesTest {
 
-    private LineSolverUtilities lineSolverUtilities;
-    private ArrayList<Square> lineEmpty;
-    private ArrayList<Square> lineFilled;
-    private ArrayList<Square> lineCrossed;
-    private ArrayList<Square> lineMixed;
-    private ArrayList<Square> lineEmptySquares;
+    private LineSolver lineSolver;
+    private Clue clue212;
 
     @BeforeEach
     void setUp() {
-        lineSolverUtilities = new LineSolverUtilities();
+        lineSolver = new LineSolver();
 
-        lineEmpty = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square()));
-
-        lineFilled = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square()));
-        lineFilled.get(0).fill();
-        lineFilled.get(1).fill();
-
-        lineCrossed = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square()));
-        lineCrossed.get(2).cross();
-
-        lineMixed = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square()));
-        lineMixed.get(0).fill();
-        lineMixed.get(1).fill();
-        lineMixed.get(2).cross();
-        lineMixed.get(3).fill();
-
-        lineEmptySquares = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square()));
-        lineEmptySquares.get(0).fill();
-        lineEmptySquares.get(1).fill();
-        lineEmptySquares.get(3).fill();
+        ArrayList<Float> brightnessLine = new ArrayList<>(Arrays.asList(0.1f, 0.1f, 0.6f, 0.1f, 0.6f, 0.1f, 0.1f));
+        clue212 = new Clue(brightnessLine);
     }
 
     @Test
-    void lineStateTests() {
-        ArrayList<Integer> expectedEmpty = new ArrayList<>(List.of(-4));
-        assertEquals(expectedEmpty, lineSolverUtilities.getLineState(lineEmpty));
+    void testLengthOfSection() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, -1, 1, 0, -2));
+        int length = lineSolver.lengthOfSection(0, 2, lineState);
+        assertEquals(4, length);
 
-        ArrayList<Integer> expectedFilled = new ArrayList<>(Arrays.asList(2, -2));
-        assertEquals(expectedFilled, lineSolverUtilities.getLineState(lineFilled));
-
-        ArrayList<Integer> expectedCrossed = new ArrayList<>(Arrays.asList(-2, 0, -1));
-        assertEquals(expectedCrossed, lineSolverUtilities.getLineState(lineCrossed));
-
-        ArrayList<Integer> expectedMixed = new ArrayList<>(Arrays.asList(2, 0, 1));
-        assertEquals(expectedMixed, lineSolverUtilities.getLineState(lineMixed));
-
-        ArrayList<Integer> expectedEmptySquares = new ArrayList<>(Arrays.asList(2, -1, 1));
-        assertEquals(expectedEmptySquares, lineSolverUtilities.getLineState(lineEmptySquares));
+        length = lineSolver.lengthOfSection(1, 4, lineState);
+        assertEquals(5, length);
     }
+
+    @Test
+    void testLengthOfIncompleteSection() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, -1, 1, 0, -2));
+        int length = lineSolver.lengthOfIncompleteSection(lineState);
+        assertEquals(5, length);
+
+        lineState = new ArrayList<>(Arrays.asList(2, -1, 1, 0, 2));
+        length = lineSolver.lengthOfIncompleteSection(lineState);
+        assertEquals(1, length);
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, -3, 2, -2, 0, 1));
+        length = lineSolver.lengthOfIncompleteSection(lineState);
+        assertEquals(7, length);
+
+        lineState = new ArrayList<>(Arrays.asList(2, 1, 0, 1, 2));
+        length = lineSolver.lengthOfIncompleteSection(lineState);
+        assertEquals(0, length);
+    }
+
+    @Test
+    void testLengthOfInterCrossSection() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, -2));
+        int length = lineSolver.lengthOfInterCrossSection(lineState);
+        assertEquals(2, length);
+
+        lineState = new ArrayList<>(Arrays.asList(2, -1, 1, 0, 2));
+        length = lineSolver.lengthOfInterCrossSection(lineState);
+        assertEquals(4, length);
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, -3, 2, -2, 0, 1));
+        length = lineSolver.lengthOfInterCrossSection(lineState);
+        assertEquals(7, length);
+
+        lineState = new ArrayList<>(Arrays.asList(2, 1, 0, 1, 2));
+        length = lineSolver.lengthOfInterCrossSection(lineState);
+        assertEquals(0, length);
+    }
+
+    @Test
+    void testFirstIncompleteClue() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, -3));
+        assertEquals(2, lineSolver.firstIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, 2));
+        assertEquals(-1, lineSolver.firstIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, 2, -1));
+        assertEquals(-1, lineSolver.firstIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, -3, 2, -2, 0, 1));
+        assertEquals(1, lineSolver.firstIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(List.of(-5));
+        assertEquals(0, lineSolver.firstIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(1, 0, 1, 0, -3));
+        assertEquals(0, lineSolver.firstIncompleteClue(clue212, lineState));
+    }
+
+    @Test
+    void testLastIncompleteClue() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, -3));
+        assertEquals(2, lineSolver.lastIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, 2));
+        assertEquals(-1, lineSolver.lastIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(-1, 2, 0, 1, 0, 2));
+        assertEquals(-1, lineSolver.lastIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, -3, 2, -2, 0, 1));
+        assertEquals(2, lineSolver.lastIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(List.of(-5));
+        assertEquals(2, lineSolver.lastIncompleteClue(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(-1, 0, 1, 0, 2));
+        assertEquals(0, lineSolver.lastIncompleteClue(clue212, lineState));
+    }
+
+    @Test
+    void testActiveClueLength() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, -3));
+        assertEquals(2, lineSolver.activeClueLength(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, 2));
+        assertEquals(0, lineSolver.activeClueLength(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, -3, 2, -2, 0, 1));
+        assertEquals(4, lineSolver.activeClueLength(clue212, lineState));
+
+        lineState = new ArrayList<>(List.of(-5));
+        assertEquals(7, lineSolver.activeClueLength(clue212, lineState));
+
+        lineState = new ArrayList<>(Arrays.asList(1, 0, 1, 0, -3));
+        assertEquals(7, lineSolver.activeClueLength(clue212, lineState));
+    }
+
+    @Test
+    void testRemoveCompletedClues() {
+        ArrayList<Integer> lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, -3));
+        ArrayList<Integer> activeClues = lineSolver.removeCompletedClues(clue212, lineState);
+        assertEquals(new ArrayList<>(List.of(2)), activeClues);
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, 1, 0, 2));
+        activeClues = lineSolver.removeCompletedClues(clue212, lineState);
+        assertEquals(new ArrayList<>(), activeClues);
+
+        lineState = new ArrayList<>(Arrays.asList(2, 0, -3, 2, -2, 0, 1));
+        activeClues = lineSolver.removeCompletedClues(clue212, lineState);
+        assertEquals(new ArrayList<>(Arrays.asList(1, 2)), activeClues);
+
+        lineState = new ArrayList<>(List.of(-5));
+        activeClues = lineSolver.removeCompletedClues(clue212, lineState);
+        assertEquals(new ArrayList<>(Arrays.asList(2, 1, 2)), activeClues);
+
+        lineState = new ArrayList<>(Arrays.asList(-1, 1, 0, 1, 0, 2));
+        activeClues = lineSolver.removeCompletedClues(clue212, lineState);
+        assertEquals(new ArrayList<>(List.of(2)), activeClues);
+    }
+
 }
