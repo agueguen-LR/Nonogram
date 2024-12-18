4a3d1b3f19b4f67328ee94fa03f6459fe0f945b7
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-12-18 19:22:00 +0100

    Extra functionalities added

--- src/nonogram/solver/LineSolver.java
+++ src/nonogram/solver/LineSolver.java
@@ -4,33 +4,73 @@ import nonogram.generator.Clue;
 import nonogram.generator.Square;
 
 import java.util.ArrayList;
-import java.util.Dictionary;
+import java.util.HashMap;
 
 /**
  * LineSolver does as much as it can on a single line of the nonogram.
  *
  * @author agueguen-LR
  */
-public class LineSolver {
+public class LineSolver extends LineSolverUtilities{
 
-    private LineSolverUtilities utilities = new LineSolverUtilities();
-    private ArrayList<Square> line;
     private ArrayList<Integer> lineState;
-    private ArrayList<Clue> clues;
+    private Clue clue;
 
     public LineSolver() {}
 
-    public void setLine(ArrayList<Square> line, ArrayList<Clue> clues) {
-        this.line = line;
-        this.lineState = utilities.getLineState(this.line);
-        this.clues = clues;
+    public void setLine(ArrayList<Square> line, Clue clue) {
+        this.lineState = getLineState(line);
+        this.clue = clue;
     }
 
-    public Dictionary<Integer, Boolean> getNewSquareStates() {
-        return null;
+    public HashMap<Integer, Boolean> getNewSquareStates() {
+        HashMap<Integer, Boolean> newSquareStates = new HashMap<>();
+        ArrayList<Integer> newFilledSquares = FillEmptySquares();
+        for(Integer squareIndex : newFilledSquares){
+            newSquareStates.put(squareIndex, true);
+        }
+        return newSquareStates;
     }
 
+    /**
+     * Returns the indexes of the squares that should be filled between each side.
+     * Can contain squares already filled.
+     * @return the indexes of the squares that should be filled
+     */
+    public ArrayList<Integer> FillEmptySquares(){
+        ArrayList<Integer> FilledSquares = new ArrayList<>();
+        int incompleteLength = lengthOfInterCrossSection(lineState);
 
+        //line is already complete
+        if(incompleteLength == 0){
+            return FilledSquares;
+        }
 
+        int stateIndex = nextEmptyIndex(0, lineState);
 
+        int clueLength = activeClueLength(clue, lineState);
+        int notCompletableLength = incompleteLength - clueLength;
+        int alreadyFilled = 0;
+
+        if (stateIndex > 0){
+            if (lineState.get(stateIndex-1) > 0){
+                alreadyFilled = lineState.get(stateIndex-1);
+            }
+        }
+
+        int squareIndex = stateIndexToSquareIndex(stateIndex, lineState) - alreadyFilled;
+
+        if (clueLength > incompleteLength/2){
+            ArrayList<Integer> activeClues = removeCompletedClues(clue, lineState);
+            for (Integer clueValue : activeClues){
+                if (clueValue > notCompletableLength){
+                    for(int i = 0; i < clueValue-notCompletableLength; i++){
+                        FilledSquares.add(squareIndex + notCompletableLength + i);
+                    }
+                }
+                squareIndex += clueValue + 1;
+            }
+        }
+        return FilledSquares;
+    }
 }
--- src/nonogram/solver/LineSolverUtilities.java
+++ src/nonogram/solver/LineSolverUtilities.java
@@ -1,10 +1,12 @@
 package nonogram.solver;
 
+import nonogram.generator.Clue;
 import nonogram.generator.Square;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class LineSolverUtilities {
+abstract class LineSolverUtilities implements StateUtilities, ClueUtilities{
 
     /**
      * Gives a comprehensive state of the line
@@ -49,6 +51,307 @@ public class LineSolverUtilities {
         return lineState;
     }
 
+    /**
+     * Converts the index of an element in the lineState to the index of the square in the line.
+     * @param stateIndex the index of the element in the lineState
+     * @param lineState the lineState
+     * @return the index of the square in the line
+     */
+    public int stateIndexToSquareIndex(int stateIndex, ArrayList<Integer> lineState){
+        int squareIndex = 0;
+        int i = 0;
+        while (i < stateIndex){
+            int stateValue = lineState.get(i);
+            if (stateValue == 0){
+                squareIndex++;
+            } else if (stateValue > 0){
+                squareIndex += stateValue;
+            } else {
+                squareIndex -= stateValue;
+            }
+            i++;
+        }
+        return squareIndex;
+    }
 
+    /**
+     * Gets the index of the next cross in lineState after currentIndex.
+     * @param currentIndex the index of the current element in the line
+     * @param lineState the lineState
+     * @return the index of the next cross in lineState after currentIndex, -1 if there is no cross
+     */
+    public int nextCrossIndex(int currentIndex, ArrayList<Integer> lineState){
+        int i = currentIndex;
+        while (i < lineState.size()){
+            if (lineState.get(i) == 0){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the index of the previous cross in lineState before currentIndex.
+     * @param currentIndex the index of the current element in the line
+     * @param lineState the lineState
+     * @return the index of the previous cross in lineState before currentIndex, -1 if there is no cross
+     */
+    public int previousCrossIndex(int currentIndex, ArrayList<Integer> lineState){
+        int i = currentIndex;
+        while (i >= 0){
+            if (lineState.get(i) == 0){
+                return i;
+            }
+            i--;
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the index of the next filled section in lineState after currentIndex.
+     * @param currentIndex the index of the current element in the line
+     * @param lineState the lineState
+     * @return the index of the next filled section in lineState after currentIndex, -1 if there is no filled section
+     */
+    public int nextFilledIndex(int currentIndex, ArrayList<Integer> lineState) {
+        int i = currentIndex;
+        while (i < lineState.size()) {
+            if (lineState.get(i) > 0) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the index of the previous filled section in lineState before currentIndex.
+     * @param currentIndex the index of the current element in the line
+     * @param lineState the lineState
+     * @return the index of the previous filled section in lineState before currentIndex, -1 if there is no filled section
+     */
+    public int previousFilledIndex(int currentIndex, ArrayList<Integer> lineState) {
+        int i = currentIndex;
+        while (i >= 0) {
+            if (lineState.get(i) > 0) {
+                return i;
+            }
+            i--;
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the index of the next empty section in lineState after currentIndex.
+     * @param currentIndex the index of the current element in the line
+     * @param lineState the lineState
+     * @return the index of the next empty section in lineState after currentIndex, -1 if there is no empty section
+     */
+    public int nextEmptyIndex(int currentIndex, ArrayList<Integer> lineState) {
+        int i = currentIndex;
+        while (i < lineState.size()) {
+            if (lineState.get(i) < 0) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the index of the previous empty section in lineState before currentIndex.
+     * @param currentIndex the index of the current element in the line
+     * @param lineState the lineState
+     * @return the index of the previous empty section in lineState before currentIndex, -1 if there is no empty section
+     */
+    public int previousEmptyIndex(int currentIndex, ArrayList<Integer> lineState) {
+        int i = currentIndex;
+        while (i >= 0) {
+            if (lineState.get(i) < 0) {
+                return i;
+            }
+            i--;
+        }
+        return -1;
+    }
+
+    /**
+     * Returns the length of the section from startIndex to endIndex in lineState.
+     * @param startIndex the index of the start of the section
+     * @param endIndex the index of the end of the section
+     * @param lineState the lineState
+     * @return the length of the section
+     */
+    public int lengthOfSection(int startIndex, int endIndex, ArrayList<Integer> lineState) {
+        int length = 0;
+        for (int i = startIndex; i <= endIndex; i++) {
+            length += Math.abs(lineState.get(i));
+            if(lineState.get(i) == 0){
+                length++;
+            }
+        }
+        return length;
+    }
+
+    /**
+     * Returns the length of the incomplete section in lineState.
+     * @param lineState the lineState
+     * @return the length of the incomplete section
+     */
+    public int lengthOfIncompleteSection(ArrayList<Integer> lineState) {
+        int startIndex = nextEmptyIndex(0, lineState);
+        int endIndex = previousEmptyIndex(lineState.size()-1, lineState);
+        if (startIndex == -1 || endIndex == -1){
+            return 0;
+        }
+        return lengthOfSection(startIndex, endIndex, lineState);
+    }
+
+    /**
+     * Returns the length of the section between crosses in lineState.
+     * @param lineState the lineState
+     * @return the length of the section between crosses
+     */
+    public int lengthOfInterCrossSection(ArrayList<Integer> lineState) {
+        int startIndex = nextEmptyIndex(0, lineState);
+        int endIndex = previousEmptyIndex(lineState.size()-1, lineState);
+        if (startIndex == -1 || endIndex == -1) {
+            return 0;
+        }
+
+        int startCrossIndex = previousCrossIndex(startIndex, lineState);
+        int endCrossIndex = nextCrossIndex(endIndex, lineState);
+        if(startCrossIndex == -1 && endCrossIndex == -1){
+            return lengthOfSection(0, lineState.size()-1, lineState);
+        } else if (startCrossIndex == -1){
+            return lengthOfSection(0, endCrossIndex-1, lineState);
+        } else if (endCrossIndex == -1){
+            return lengthOfSection(startCrossIndex+1, lineState.size()-1, lineState);
+        }
+        return lengthOfSection(startCrossIndex+1, endCrossIndex-1, lineState);
+    }
+
+
+
+
+    /**
+     * Returns the index of the first incomplete clue in the Clue.
+     * @param clue the clue
+     * @return the index of the first incomplete clue
+     */
+    public int firstIncompleteClue(Clue clue, ArrayList<Integer> lineState){
+        int filledIndex = nextFilledIndex(0, lineState);
+        int emptyIndex = nextEmptyIndex(0, lineState);
+
+        //No empty squares means the line is complete and all clues are filled
+        if(emptyIndex == -1){
+            return -1;
+        }
+        //Nothing is filled so the first clue is incomplete
+        if (filledIndex == -1){
+            return 0;
+        }
+
+        int stateValue = lineState.get(filledIndex);
+        int clueIndex = 0;
+
+        //while filled sections are equal to clues and no empty square has been passed
+        while(filledIndex < emptyIndex && stateValue == clue.getClue().get(clueIndex)){
+
+            clueIndex++;
+            //all clues are complete
+            if (clueIndex == clue.getClue().size()){
+                return -1;
+            }
+
+            filledIndex = nextFilledIndex(filledIndex+stateValue, lineState);
+            //no more filled squares
+            if (filledIndex == -1){
+                return clueIndex;
+            }
+            stateValue = lineState.get(filledIndex);
+        }
+        //clue is incomplete or empty square has been passed
+        return clueIndex;
+    }
+
+    /**
+     * Returns the index of the last incomplete clue in the Clue.
+     * @param clue the clue
+     * @return the index of the last incomplete clue
+     */
+    public int lastIncompleteClue(Clue clue, ArrayList<Integer> lineState) {
+        int filledIndex = previousFilledIndex(lineState.size()-1, lineState);
+        int emptyIndex = previousEmptyIndex(lineState.size()-1, lineState);
+
+        //No empty squares means the line is complete and all clues are filled
+        if(emptyIndex == -1){
+            return -1;
+        }
+        //Nothing is filled so the last clue is incomplete
+        if (filledIndex == -1){
+            return clue.getClue().size()-1;
+        }
+
+        int stateValue = lineState.get(filledIndex);
+        int clueIndex = clue.getClue().size()-1;
+
+        //while filled sections are equal to clues and no empty square has been passed
+        while(filledIndex > emptyIndex && stateValue == clue.getClue().get(clueIndex)){
+
+            clueIndex--;
+            //all clues are complete
+            if (clueIndex == -1){
+                return -1;
+            }
+
+            filledIndex = previousFilledIndex(filledIndex-stateValue, lineState);
+            //no more filled squares
+            if (filledIndex == -1){
+                return clueIndex;
+            }
+            stateValue = lineState.get(filledIndex);
+        }
+        //clue is incomplete or empty square has been passed
+        return clueIndex;
+    }
+
+    /**
+     * Returns the length of the filled squares + in between crosses inferred by the clue
+     * between the first and last incomplete clues.
+     * @param clue the clue
+     * @return the total length
+     */
+    public int activeClueLength(Clue clue, ArrayList<Integer> lineState) {
+        int firstIncomplete = firstIncompleteClue(clue, lineState);
+        int lastIncomplete = lastIncompleteClue(clue, lineState);
+
+        if (firstIncomplete == -1 || lastIncomplete == -1){
+            return 0;
+        }
+
+        int length = 0;
+        for (int i = firstIncomplete; i <= lastIncomplete; i++){
+            length += clue.getClue().get(i);
+        }
+        return length + lastIncomplete - firstIncomplete;
+    }
+
+
+    /**
+     * Removes the completed clues from the clue.
+     * @param clue the clue
+     * @param lineState the lineState
+     * @return the clue without the completed clues
+     */
+    public ArrayList<Integer> removeCompletedClues(Clue clue, ArrayList<Integer> lineState) {
+        int startIndex = firstIncompleteClue(clue, lineState);
+        int endIndex = lastIncompleteClue(clue, lineState);
+        if (startIndex == -1 || endIndex == -1){
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(clue.getClue().subList(startIndex, endIndex + 1));
+    }
 
 }
