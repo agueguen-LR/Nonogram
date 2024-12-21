e1dc098999671333e8c23dbdc51cdf096b3927d7
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-12-21 17:16:33 +0100

    added new part of solving algorithm

--- src/nonogram/solver/LineSolver.java
+++ src/nonogram/solver/LineSolver.java
@@ -13,31 +13,42 @@ import java.util.HashMap;
  */
 public class LineSolver extends LineSolverUtilities{
 
-    private ArrayList<Integer> lineState;
-    private Clue clue;
-
     public LineSolver() {}
 
-    public void setLine(ArrayList<Square> line, Clue clue) {
-        this.lineState = getLineState(line);
-        this.clue = clue;
-    }
+    /**
+     * Returns the new states of the squares in the line after as much as possible has been solved.
+     * @param line the line to solve
+     * @param clue the clue for the line
+     * @return HashMap of the new states of the squares, with the index of the square as the key and value as a boolean, true if filled, false if crossed
+     */
+    public HashMap<Integer, Boolean> getNewSquareStates(ArrayList<Square> line, Clue clue) {
+        ArrayList<Integer> lineState = getLineState(line);
 
-    public HashMap<Integer, Boolean> getNewSquareStates() {
         HashMap<Integer, Boolean> newSquareStates = new HashMap<>();
-        ArrayList<Integer> newFilledSquares = FillEmptySquares();
-        for(Integer squareIndex : newFilledSquares){
+        ArrayList<Integer> spaceFiller = SpaceFiller(lineState, clue);
+        ArrayList<Integer> edgeExtender = edgeExtender(line, lineState, clue);
+        ArrayList<Integer> edgeExtenderReversed = edgeExtender(new ArrayList<>(line.reversed()), new ArrayList<>(lineState.reversed()), clue.reverse());
+        for(Integer squareIndex : spaceFiller){
+            if(line.get(squareIndex).isEmpty()){
+                newSquareStates.put(squareIndex, true);
+            }
+        }
+        for(Integer squareIndex : edgeExtender){
             newSquareStates.put(squareIndex, true);
         }
+//        for(Integer squareIndex : edgeExtenderReversed){
+//            newSquareStates.put(line.size()-squareIndex, true);
+//        }
         return newSquareStates;
     }
 
     /**
      * Returns the indexes of the squares that should be filled between each side.
+     * Based upon the space between the edges and the total length of active clues.
      * Can contain squares already filled.
      * @return the indexes of the squares that should be filled
      */
-    public ArrayList<Integer> FillEmptySquares(){
+    public ArrayList<Integer> SpaceFiller(ArrayList<Integer> lineState, Clue clue){
         ArrayList<Integer> FilledSquares = new ArrayList<>();
         int incompleteLength = lengthOfInterCrossSection(lineState);
 
@@ -72,5 +83,58 @@ public class LineSolver extends LineSolverUtilities{
             }
         }
         return FilledSquares;
+    }
+
+    /**
+     * Extends the filled squares based on distance from the edge, goes leftwards
+     * @param lineState the state of the line
+     * @param clue the clue for the line
+     * @return the indexes of the squares that should be filled
+     */
+    public ArrayList<Integer> edgeExtender(ArrayList<Square> line, ArrayList<Integer> lineState, Clue clue) {
+        ArrayList<Integer> newSquares = new ArrayList<>();
+
+        int currentClueIndex = firstIncompleteClue(clue, lineState);
+        if (currentClueIndex == -1) { // no incomplete clues
+            return newSquares;
+        }
+
+        int emptyIndex = nextEmptyIndex(0, lineState);
+        int previousFilledIndex = previousFilledIndex(emptyIndex, lineState);
+        int nextFilledIndex = nextFilledIndex(emptyIndex, lineState);
+
+        if (emptyIndex == -1) { // line is already complete, theoretically impossible to reach because firstIncompleteClue would return -1
+            return newSquares;
+        }
+
+        int AmountOfSquaresToFill;
+        int squareIndex;
+
+        if (previousFilledIndex != -1 && previousFilledIndex == emptyIndex - 1) { //Extension starts from filled square
+            AmountOfSquaresToFill = clue.getClue().get(currentClueIndex) - lineState.get(emptyIndex - 1);
+            squareIndex = stateIndexToSquareIndex(emptyIndex, lineState);
+
+        } else if(nextFilledIndex == -1){ //No filled square to extend from
+            return newSquares;
+
+        }else{ // Extension has a gap at the start
+            AmountOfSquaresToFill = clue.getClue().get(currentClueIndex) - (lineState.get(emptyIndex + 1)-lineState.get(emptyIndex));
+            squareIndex = stateIndexToSquareIndex(emptyIndex+1, lineState) + lineState.get(emptyIndex+1);
+        }
+
+        if(AmountOfSquaresToFill <= 0){ // no squares can be filled with this method
+            return newSquares;
+        }
+
+        while (squareIndex < line.size() && AmountOfSquaresToFill > 0){
+            if(line.get(squareIndex).isCrossed()){ // hit an edge, fill backwards TO DO
+                break;
+            }
+            newSquares.add(squareIndex);
+            squareIndex++;
+            AmountOfSquaresToFill--;
+        }
+
+        return newSquares;
     }
 }
--- test/nonogram/solver/LineSolverTest.java
+++ test/nonogram/solver/LineSolverTest.java
@@ -7,24 +7,36 @@ import nonogram.generator.Square;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class LineSolverTest {
     
     private LineSolver lineSolver;
+
     private ArrayList<Square> lineEmpty;
     private ArrayList<Square> lineFilled;
     private ArrayList<Square> lineCrossed;
     private ArrayList<Square> lineMixed;
     private ArrayList<Square> lineEmptySquares;
+
     private ArrayList<Square> line574;
-    private Clue clue212;
+    private ArrayList<Square> line44;
+    private ArrayList<Square> line44filledStart;
+    private ArrayList<Square> line44crossedStart;
+    private ArrayList<Square> line44noPossibleExtension;
+    private ArrayList<Square> line44noNeededExtension;
+
     private Clue clue574;
+    private Clue clue44;
+    private Clue clue21;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<Clue> clueConstructor = Clue.class.getDeclaredConstructor(ArrayList.class, int.class);
+        clueConstructor.setAccessible(true);
+
         lineSolver = new LineSolver();
 
         lineEmpty = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square()));
@@ -48,12 +60,29 @@ public class LineSolverTest {
         lineEmptySquares.get(3).fill();
 
         line574 = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square(),new Square(), new Square(), new Square(), new Square(),new Square(), new Square(), new Square(), new Square(),new Square(), new Square(), new Square(), new Square(),new Square(), new Square(), new Square(), new Square()));
+        clue574 = clueConstructor.newInstance(new ArrayList<>(Arrays.asList(5, 7, 4)), 1234);
 
-        ArrayList<Float> brightnessLine = new ArrayList<>(Arrays.asList(0.1f, 0.1f, 0.6f, 0.1f, 0.6f, 0.1f, 0.1f));
-        clue212 = new Clue(brightnessLine);
+        line44 = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square()));
+        line44.get(1).fill();
+        line44.get(8).fill();
+        line44filledStart = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square()));
+        line44filledStart.get(0).fill();
+        line44filledStart.get(9).fill();
+        line44crossedStart = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square()));
+        line44crossedStart.get(0).cross();
+        line44crossedStart.get(2).fill();
+        line44noPossibleExtension = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square()));
+        line44noPossibleExtension.get(3).fill();
+        line44noNeededExtension = new ArrayList<>(Arrays.asList(new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square()));
+        line44noNeededExtension.get(0).fill();
+        line44noNeededExtension.get(1).fill();
+        line44noNeededExtension.get(2).fill();
+        line44noNeededExtension.get(3).fill();
+        clue44 = clueConstructor.newInstance(new ArrayList<>(Arrays.asList(4, 4)), 1234);
 
-        brightnessLine = new ArrayList<>(Arrays.asList(0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.6f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.6f, 0.1f, 0.1f, 0.1f, 0.1f));
-        clue574 = new Clue(brightnessLine);
+        clue21 = clueConstructor.newInstance(new ArrayList<>(Arrays.asList(2, 1)), 1234);
+
+
     }
 
     @Test
@@ -76,12 +105,44 @@ public class LineSolverTest {
 
     @Test
     void testFillEmptySquares(){
-        lineSolver.setLine(line574, clue574);
-        ArrayList<Integer> filledSquares = lineSolver.FillEmptySquares();
+        ArrayList<Integer> filledSquares = lineSolver.SpaceFiller(lineSolver.getLineState(line574), clue574);
         ArrayList<Integer> expectedFilledSquares = new ArrayList<>(Arrays.asList(2, 3, 4, 8, 9, 10, 11, 12, 16, 17));
         assertEquals(expectedFilledSquares, filledSquares);
+    }
 
+    @Test
+    void testEdgeExtender(){
+        ArrayList<Integer> filledSquares = lineSolver.edgeExtender(line44, lineSolver.getLineState(line44), clue44);
+        ArrayList<Integer> expectedFilledSquares = new ArrayList<>(Arrays.asList(2, 3));
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
 
+        filledSquares = lineSolver.edgeExtender(line44filledStart, lineSolver.getLineState(line44filledStart), clue44);
+        expectedFilledSquares = new ArrayList<>(Arrays.asList(1, 2, 3));
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
+
+        filledSquares = lineSolver.edgeExtender(lineMixed, lineSolver.getLineState(lineMixed), clue21);
+        expectedFilledSquares = new ArrayList<>();
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
+
+        filledSquares = lineSolver.edgeExtender(lineEmpty, lineSolver.getLineState(lineEmpty), clue21);
+        expectedFilledSquares = new ArrayList<>();
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
+
+        filledSquares = lineSolver.edgeExtender(line44crossedStart,lineSolver.getLineState(line44crossedStart), clue44);
+        expectedFilledSquares = new ArrayList<>(Arrays.asList(3, 4));
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
+
+        filledSquares = lineSolver.edgeExtender(line44noPossibleExtension, lineSolver.getLineState(line44noPossibleExtension), clue44);
+        expectedFilledSquares = new ArrayList<>();
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
+
+        filledSquares = lineSolver.edgeExtender(lineFilled, lineSolver.getLineState(lineFilled), clue21);
+        expectedFilledSquares = new ArrayList<>();
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
+
+        filledSquares = lineSolver.edgeExtender(line44noNeededExtension, lineSolver.getLineState(line44noNeededExtension), clue44);
+        expectedFilledSquares = new ArrayList<>();
+        assertEquals(expectedFilledSquares, new ArrayList<>(filledSquares));
     }
     
 }
